9457ab1628050ade397a5259de2aa4509c79d861
MJ O'Neill <91608528+MJ-ONeill@users.noreply.github.com>
2023-05-27 18:22:13 +0200

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# Fixes a wordpress site running on apache2
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+#fix the configuration file for Apache web server so that it points to the correct WordPress
+exec { 'fix config file':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }

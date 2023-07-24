478e908e5857736e66334f61ece0c4802bb1f730
MJ-ONEill <mjoneill20100@gmail.com>
2023-07-24 07:26:06 -0700

    0- gather data rest api update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 """
-Write a Python script using REST API to gather employe data"""
+Write a Python script using REST API to gather employee data"""
 import requests
 import sys
 
-if __name__ == "main__":
+if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     user_url = url + "users/{}".format(sys.argv[1])
     todo_url = url + "todos"
-    params = {"user_ID: sys.arv[1]"}
+    params = {"userId": sys.argv[1]}
     user = requests.get(url=user_url).json()
-    todos = requests.get(url=todo_url, paramas=params).json()
-    completed = []
+    todos = requests.get(url=todo_url, params=params).json()
+    completed = sum(1 for todo in todos if todo.get("completed"))
+
+    print("Employee {} is done with tasks ({}/{}):".format(
+        user.get("name"), completed, len(todos)))
 
     for todo in todos:
-        if todo.get("completed") is True:
-            completed.append(todo.get("title"))
-
-        print("Employee {} is done with tasks({}/{}):"
-              .format(user.get("name"), len(completed), len(todos)))
-
-        for complete in completed:
-            print("\t {}".format(complete))
+        if todo.get("completed"):
+            print("\t{}".format(todo.get("title")))
